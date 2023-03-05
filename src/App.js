417bfb43bed80383ea9598e5dcaa9836a7e6e5f5
Martin McInerney <martin_mcinerney@live.ie>
2023-03-05 20:04:35 +0000

    Adds homepage sign in container

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import './App.css';
+import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import { Container } from 'react-bootstrap';
 
 
 
 function App() {
   return (
-    <div className="App">
+    
+    <div>
       <NavBar />
+      <Container className={styles.Main}>
+        <h1>Homepage</h1>
+        <h1>Sign in</h1>
+      </Container>
     </div>
+    
   );
 }
 
