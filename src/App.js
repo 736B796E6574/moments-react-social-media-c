44f5196bfc4b6a8f5edb95ae7ad4af1a22ab97ce
martin-mcinerney <martin_mcinerney@live.ie>
2023-03-05 20:47:26 +0000

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import { Container } from 'react-bootstrap';
+import { Route, Switch } from 'react-router-dom';
 
 
 
@@ -10,8 +11,14 @@ function App() {
     <div>
       <NavBar />
       <Container className={styles.Main}>
-        <h1>Homepage</h1>
-        <h1>Sign in</h1>
+        <Switch>
+          <Route exact path="/" render={()=> <h1>Homepage</h1>}/>
+          <Route exact path="/signin" render={()=> <h1>Sign in</h1>}/>
+          <Route exact path="/signup" render={()=> <h1>Sign up</h1>}/>
+          <Route render={() => <p>Page not found!</p>} />
+        </Switch>
+        
+        
       </Container>
     </div>
     
