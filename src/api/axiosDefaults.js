96efeff38f1efb7d84ecca32f10e926f8bf6945d
martin-mcinerney <martin_mcinerney@live.ie>
2023-03-06 20:00:04 +0000

    JWT refactor

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,7 @@ import axios from "axios";
 axios.defaults.baseURL = "https://drf-api-1010.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+import axios from "axios";
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
