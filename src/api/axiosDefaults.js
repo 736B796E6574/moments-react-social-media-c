2331923a9260fe02c985b992164b083652d72e81
Martin McInerney <martin_mcinerney@live.ie>
2023-03-07 23:07:22 +0000

    api endpoint changed

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://drf-api-1010.herokuapp.com/";
+axios.defaults.baseURL = "https://drf-2.herokuapp.com";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
