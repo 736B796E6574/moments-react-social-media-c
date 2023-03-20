aa628d12db09d59ab8bae14ffedea1682ecfbaf9
martin-mcinerney <martin_mcinerney@live.ie>
2023-03-20 13:01:31 +0000

    Adds postspage and routes

--- src/App.js
+++ src/App.js
@@ -7,14 +7,21 @@ import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostPage from "./pages/posts/PostPage";
+import PostsPage from "./pages/posts/PostsPage";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 function App() {
+  const currentUser = useCurrentUser;
+  const profile_id = currentUser?.profile_id || "";
+
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/" render={() => <PostsPage message="No results found. Adjust the seacrh keywords."/>} />
+          <Route exact path="/feed" render={() => <PostsPage message="No results found. Adjust the search keywords." filter={`owner__followed__owner__profile=${profile_id}&`} />} />
+          <Route exact path="/liked" render={() => <PostsPage message="No results found. Adjust the seacrh keywords or like a post."/>} />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
